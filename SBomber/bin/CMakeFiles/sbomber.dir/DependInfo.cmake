5689b4c22fc083821da87b742a3a9a9136f68370
maxim <maxim_sdrv@mail.ru>
2021-12-20 07:47:17 +0300

    added project Sbomber crossplatform

---
+++ SBomber/bin/CMakeFiles/sbomber.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/Bomb.cpp" "CMakeFiles/sbomber.dir/src/Bomb.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/Bomb.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/Crater.cpp" "CMakeFiles/sbomber.dir/src/Crater.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/Crater.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/Ground.cpp" "CMakeFiles/sbomber.dir/src/Ground.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/Ground.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/House.cpp" "CMakeFiles/sbomber.dir/src/House.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/House.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/LevelGUI.cpp" "CMakeFiles/sbomber.dir/src/LevelGUI.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/LevelGUI.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/MyTools.cpp" "CMakeFiles/sbomber.dir/src/MyTools.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/MyTools.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/Plane.cpp" "CMakeFiles/sbomber.dir/src/Plane.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/Plane.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/SBomber.cpp" "CMakeFiles/sbomber.dir/src/SBomber.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/SBomber.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/ScreenSingleton.cpp" "CMakeFiles/sbomber.dir/src/ScreenSingleton.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/ScreenSingleton.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/Tank.cpp" "CMakeFiles/sbomber.dir/src/Tank.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/Tank.cpp.o.d"
+  "/Users/lllshamanlll/projects/GeekBrains/Patterns/SBomber/SBomberProject/src/main.cpp" "CMakeFiles/sbomber.dir/src/main.cpp.o" "gcc" "CMakeFiles/sbomber.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SBomber/src/main.cpp
@@ -0,0 +1,65 @@
+﻿#include "SBomber.h"
+#include "MyTools.h"
+#include "ScreenSingleton.h"
+
+#if defined(_WIN32) || defined(WIN32)
+
+//#include <conio.h>
+//
+#else
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+
+int _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (! initialized) {
+        // Use termios to turn off line buffering
+        termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+}
+
+#endif
+
+
+int main(void) {
+  MyTools::OpenLogFile("log.txt");
+
+  SBomber game;
+
+  do {
+    game.TimeStart();
+
+    if (_kbhit()) {
+      game.ProcessKBHit();
+    }
+
+    ScreenSingleton::getInstance().ClrScr();
+
+    game.DrawFrame();
+    game.MoveObjects();
+    game.CheckObjects();
+
+    game.TimeFinish();
+
+  } while (!game.GetExitFlag());
+
+  MyTools::CloseLogFile();
+
+  return 0;
+}
